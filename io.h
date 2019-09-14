1bc62e55a5d9e2f9c2149a3a813ad787f2020073
Fernando Ferreira Silva <fernando@ferreirasil.com>
2019-09-14 14:43:44 -0300

    io

---
+++ io.h
@@ -0,0 +1,21 @@
+#ifndef IO_H
+#define IO_H
+#include <stdint.h>
+
+#define EN_IO_LATCH RC4
+#define LATCH_BUS PORTD
+
+typedef enum {
+    BLUE,
+    GREEN,
+    RED,
+    CYAN,
+    MAGENTA,
+    YELLOW
+            
+}BackgroundColor;
+
+void setDisplayBack(BackgroundColor color);
+void refreshIoLatch(void);
+
+#endif
